2d0804962cf840f8ffbc4166cadeda537837cde1
GulnarIbisova 2024 <ibishova.gulnar+demo2@gmail.com>
2024-03-20 21:12:42 +0000

    To-Do

--- NewStart 1/app.js
+++ NewStart 1/app.js
@@ -1,4 +1,16 @@
 "use strict";
 $(document).ready(function () {
-
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
+            let c = d => {
+                for (let i = 0; i < d; i++) {
+                    $('.minitbox').append(`<h3>${d}</h3>`);
+                }
+            }
+            let a = b => Number(b) ? c(b) : $('.minibox').append(`<h3>${b}</h3>`);
+            a($('input').val());
+        }
+        $('input').val('')
+    });
 });
